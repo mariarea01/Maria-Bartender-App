aebe6eab359572081a3868b5be7ad93584bdb396
mariarea01 <66438113+mariarea01@users.noreply.github.com>
2022-08-29 21:10:49 -0400

    Bartender-App

---
+++ Bartender-App/Bartender-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bartender_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
